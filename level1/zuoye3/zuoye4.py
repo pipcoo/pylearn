d0dba5d27e04c64992a4537ef74b5c5bc81c6949
吴峰 <pipcoo@126.com>
2017-03-29 01:06:09 +0800

    commit 作业3

--- level1/zuoye3/zuoye4.py
+++ level1/zuoye3/zuoye4.py
@@ -2,20 +2,72 @@
 # -*- coding: utf-8 -*-
 # @Author: wufeng
 
+def append_list(file,read_count):
+    file2list = []
+    for line in file:
+        linesize=0
+        read_flag=True
+        read_count+=len(line)
+        if len(line)>1:
+            linesize=len(line)
+            line2list=line.replace("\r\n"," ").split()
+            if line2list[0]=='backend' and read_flag:
+                seeknum[0]-=linesize
+                read_flag=False
+                break
+            elif line2list[0]=='backend' and not read_flag:
+                file2list.append(line)
+                read_flag = True
+            else:
+                file2list.append(line)
+                #seeknum[1] = 1
+    return file2list,read_count
+
 def read_cfg():
 
     f=open("haproxy.cfg","r",encoding="utf-8")
+    read_count=0
+    backend_list=[]
     for line in f:
-
+        list1,read_count=append_list(f,read_count)
+        read_count+=len(line)
+        backend_list.append(list1)
+        f.seek(read_count - len(line))
     f.close()
+    return backend_list
 
-a='backend www.oldboy.org'
-b='        server 100.1.7.9 100.1.7.9 weight 20 maxconn 3000'
-c=list(b)
-d=b[0:8]
+seeknum=[0,0]
+a=read_cfg()
 
-if d == '        ':
-    print(True)
-
-print(b.split())
-print(d)
+for i in range(1,len(a)):
+    print(a[i])
+#
+# def read_cfg():
+#     file2list = []
+#     f=open("haproxy.cfg","r",encoding="utf-8")
+#
+#     a=[]
+#     serverlist = []
+#     for line in f:
+#        key=""
+#
+#         seeknum[0]+=len(line)
+#         if(line.startswith(backend)){
+#         if(serverlist.length>1){
+#         file2list.append()
+#         }
+#         key = line.split("")[1]
+#         serverlist=[]
+#         }else{
+#         serverlist.append(line)
+#
+#         }
+#
+#
+#         a.append(append_list(f))
+#         f.seek(seeknum[0] - len(line))
+#
+# [key1:{'',''}]
+#
+#     f.close()
+#     return a
