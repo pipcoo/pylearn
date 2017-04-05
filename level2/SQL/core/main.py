dc9f53ada549369fb594f7308c5eb6609934185f
吴峰 <pipcoo@126.com>
2017-04-05 23:53:38 +0800

    level2 1

---
+++ level2/SQL/core/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+@version: 0.1
+@author: wufeng
+@contact: pipcoo@pipcoo.com
+@site: http://www.pipcoo.com
+@software: PyCharm
+@file: main.py
+@time: 2017/4/5 23:48
+"""
+
+
+def sqlplus():
+    print('111')
---
+++ level2/SQL/data/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+@version: 0.1
+@author: wufeng
+@contact: pipcoo@pipcoo.com
+@site: http://www.pipcoo.com
+@software: PyCharm
+@file: __init__.py.py
+@time: 2017/4/5 22:24
+"""
+
