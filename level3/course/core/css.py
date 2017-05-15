a6013b75ba0054291597f929cad79d40309e09f2
pipcoo <pipcoo@hotmail.com>
2017-05-15 18:47:50 +0800

    css

--- level3/course/core/css.py
+++ level3/course/core/css.py
@@ -7,4 +7,27 @@ sys.path.append(BASE)
 
 from config.logger import log
 
-log.debug('start ...')
+
+admin_level1 = '''
+    1、学校管理
+    2、班级管理
+    3、课程管理
+    4、讲师管理
+'''
+
+class School(object):
+    pass
+
+class Teacher(object):
+    pass
+
+class Student(object):
+    pass
+
+class Display(object):
+    def __init__(self,menu_tital,menu_list):
+        self.menu_tital = menu_tital
+        self.menu_list = menu_list
+
+    def menu_select(self):
+        print(self.menu_tital)
