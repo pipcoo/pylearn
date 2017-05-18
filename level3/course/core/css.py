ffcf41ae35108fe461925f6849f8929aad50974a
wufeng <pipcoo@hotmail.com>
2017-05-18 23:45:07 +0800

    add

--- level3/course/core/css.py
+++ level3/course/core/css.py
@@ -7,53 +7,47 @@ sys.path.append(BASE)
 
 from config.logger import log
 
-
 menu = {
     '后台管理':{
         '学校管理':{
-            "查看学校":None,
-            "添加学校":None,
-            "删除学校":None
+            "查看学校":'',
+            "添加学校":'',
+            "删除学校":''
         },
         '班级管理':{
-            "查看班级": None,
-            "添加班级": None,
-            "删除班级": None
+            "查看班级": '',
+            "添加班级": '',
+            "删除班级": ''
         },
         '课程管理':{
-            "查看课程": None,
-            "添加课程": None,
-            "删除课程": None
+            "查看课程": '',
+            "添加课程": '',
+            "删除课程": ''
         },
         '讲师管理':{
-            "查看讲师": None,
-            "添加讲师": None,
-            "删除讲师": None
+            "查看讲师": '',
+            "添加讲师": '',
+            "删除讲师": ''
         }
     },
     '讲师':{
-        '管理班级':None,
-        '选择上课班级':None,
-        '查看班级学员列表':None,
-        '修改学员成绩':None
+        '管理班级':'',
+        '选择上课班级':'',
+        '查看班级学员列表':'',
+        '修改学员成绩':''
     },
     '学员':{
-        '注册':None,
-        '交学费':None,
-        '选择班级':None
+        '注册':'',
+        '交学费':'',
+        '选择班级':''
     }
 }
 
-class School(object):
-    pass
 
-class Teacher(object):
-    pass
-
-class Student(object):
-    pass
-
-class Display(object):
+class Menu(object):
+    """
+    菜单类
+    """
 
     def __init__(self,menu):
         self.menu = menu
@@ -61,34 +55,90 @@ class Display(object):
         self.menu_list.append(self.menu)
 
     def print_menu(self):
+        """
+        打印菜单
+        :return: 
+        """
         dis_template = '\t%d、%s'
         menu_num = 1
-        current_menu = {}
-        print_menu = self.menu
-        if len(self.menu_list) > 0 :
-            for i in self.menu_list:
-                print_menu = print_menu[i]
-        for key in print_menu:
+        current_menu = self.menu_list[-1]
+        current_level = {}
+        for key in current_menu:
             print(dis_template %(menu_num,key))
-            current_menu[str(menu_num)] = key
+            current_level[str(menu_num)] = key
             menu_num += 1
-        return current_menu
+        else:
+            print(dis_template % (0, '退出'))
+
+        return current_level,current_menu
 
     def select_menu(self):
-        current_menu = self.print_menu()
+        """
+        选择菜单
+        :return: 
+        """
+        current_level,current_menu = self.print_menu()
         exit_flag = False
         while not exit_flag:
             _input = input('输入选择的编码：\n>>')
-            if _input == 'q':
-                exit_flag = True
-            elif _input in current_menu :
-                self.menu_list.append(current_menu[_input])
-                if
+            if _input == '0':
+                if len(self.menu_list) == 1:
+                    exit_flag = True
+                else:
+                    self.menu_list.pop()                                        #删除一级目录
+                    return self.select_menu()                                   #递归调用
+            elif _input in current_level :
+                if isinstance(current_menu[current_level[_input]],str):         #判断value是否是字符串 是则到底 返回
+                    return current_menu[current_level[_input]]
+                else:
+                    self.menu_list.append(current_menu[current_level[_input]])  #添加当前级别目录
+                    return self.select_menu()                                   #递归调用 选择当前级别目录
             else:
                 print('请输入正确的编码')
 
-a = Display(menu)
+
+
+class School(object):
+    """
+    学校类
+    """
+    school_list = []
+
+    def create_school(self,school_name,school_area):
+
+
+class Class(School):
+    """
+    班级类
+    """
+    def __init__(self,school_name,school_area):
+        self.school_name = school_name
+        self.school_area = school_area
+class Course(object):
+    """
+    课程类
+    """
+    pass
+
+class School_People(object):
+    """
+    学校的人类
+    """
+    def __init__(self,school_name):
+        self.school_name = school_name
+
+    def
+class Teacher(School_People):
+    """
+    老师类
+    """
+    pass
+
+class Student(School_People):
+    """
+    学生类
+    """
+    pass
+
+a = Menu(menu)
 a.select_menu()
-
-
-
